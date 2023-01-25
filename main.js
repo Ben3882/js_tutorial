89f5fe31f7e3b25abb0cf69c52d1b1192f19937a
Ben3882 <benbrown3882@gmail.com>
2023-01-25 00:13:41 -0700

    palindrome tester html, main, and bundle.js added

---
+++ main.js
@@ -0,0 +1,11 @@
+let Phrase = require("mhartl-palindrome");
+
+let string = prompt("Enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+    alert(`${phrase.content} is a palindrome`);
+} else {
+    alert(`${phrase.content} is not a plaindrome`);
+}
+//alert(new Phrase("Madam, I'm Adam.").palindrome());
