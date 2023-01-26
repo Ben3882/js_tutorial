724757921f3d1c2ff1b5a40ce0436010ef85affe
Ben3882 <benbrown3882@gmail.com>
2023-01-26 10:12:06 -0700

    the ultimate palindrome detector

--- bundle.js
+++ bundle.js
@@ -1,14 +1,32 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("ben3882-palindrome");
 
-let string = prompt("Enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester (e) {
+    e.preventDefault();
+    let string = e.target.phrase.value;
+    let phrase = new Phrase(string);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-    alert(`${phrase.content} is a palindrome`);
-} else {
-    alert(`${phrase.content} is not a plaindrome`);
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> IS a palindrome`;
+    } else {
+        palindromeResult.innerHTML = `<strong>("${phrase.content}"</strong> is NOT a plaindrome)`;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function (e) {
+        palindromeTester(e);
+    })
+    /*let form = document.querySelector("#palindromeTester");
+    form.addEventListener("submit", function() {
+        palindromeTester();
+    }) */
+
+})
+
+
 //alert(new Phrase("Madam, I'm Adam.").palindrome());
 
 },{"ben3882-palindrome":2}],2:[function(require,module,exports){
--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 let Phrase = require("ben3882-palindrome");
 
-let string = prompt("Enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester (e) {
+    e.preventDefault();
+    let string = e.target.phrase.value;
+    let phrase = new Phrase(string);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-    alert(`${phrase.content} is a palindrome`);
-} else {
-    alert(`${phrase.content} is not a plaindrome`);
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> IS a palindrome`;
+    } else {
+        palindromeResult.innerHTML = `<strong>("${phrase.content}"</strong> is NOT a plaindrome)`;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function (e) {
+        palindromeTester(e);
+    })
+    /*let form = document.querySelector("#palindromeTester");
+    form.addEventListener("submit", function() {
+        palindromeTester();
+    }) */
+
+})
+
+
 //alert(new Phrase("Madam, I'm Adam.").palindrome());
